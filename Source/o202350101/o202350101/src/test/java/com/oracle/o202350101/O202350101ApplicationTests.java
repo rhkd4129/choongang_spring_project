6cd0740b85e35aa3d6659669381596c50f4bd727
이광현 <rhkd4129@naver.com>
2023-10-26 11:12:02 +0900

    one start

---
+++ Source/o202350101/o202350101/src/test/java/com/oracle/o202350101/O202350101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.o202350101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O202350101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
