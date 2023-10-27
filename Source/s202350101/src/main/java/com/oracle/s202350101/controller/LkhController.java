bc06171de82f9e9f75e2d160d437a6c9375cfb51
이광현 <rhkd4129@naver.com>
2023-10-27 10:24:08 +0900

    a

--- Source/s202350101/src/main/java/com/oracle/s202350101/controller/LkhController.java
+++ Source/s202350101/src/main/java/com/oracle/s202350101/controller/LkhController.java
@@ -1,5 +1,22 @@
 package com.oracle.s202350101.controller;
 
-public class LkhController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-}
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class LkhController {
+	
+	
+	@GetMapping("Lee")
+	public String Hello() {
+		System.out.println("Hello");
+		
+		return "project/board/Hello";
+	}
+	
+} 
